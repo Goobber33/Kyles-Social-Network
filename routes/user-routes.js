52253f0f04178ce7bd2f59ac8a289f4dc6b80edd
Kyle <kcparks1234@gmail.com>
2023-05-02 08:57:24 -0700

    Added code for thought-routes, and added thought-controller file

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,7 +7,7 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-} = require('../controllers/user.controller');
+} = require('../controllers/user-controller');
 
 // Set up routes for /api/users
 
--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/snetwork', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 
 app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+  
